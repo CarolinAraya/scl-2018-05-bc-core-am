58f38962de050d0920573ffde32bbe16a562f35e
Carolina Araya A <_carolina_@hotmail.cl>
2018-05-27 15:29:05 -0400

    DOM, falta agregar un boton clean

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 /* Acá va tu código */
 // solo manejo del DOM
+let offset = document.getElementById('offsetNumberInput');
+let naturalText = document.getElementById('naturalText');
+let encryptedText = document.getElementById('encryptedText');
+
+document.getElementById('encryptionButton').addEventListener('click', function encrypt() {
+  window.cipher.encode();
+})
+
+document.getElementById('decryptButton').addEventListener('click', function decrypt() {
+    window.cipher.decode();
+})

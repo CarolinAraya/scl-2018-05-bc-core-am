54e0c66763472695eaf917a99d7980b4006d005b
carolinaraya <_carolina_@hotmail.cl>
2018-05-30 12:56:16 -0400

    if para el caso de espacio (32ascii)

--- src/cipher.js
+++ src/cipher.js
@@ -2,22 +2,32 @@
 
 window.cipher = {
   encode: (text, offset) => {
-    var result = "";
+    let result = "";
 
-    for (var i = 0; i < text.length; i++) {
+    for (let i = 0; i < text.length; i++) {
       //var inputAscii = text.charCodeAt(i);
-      var outputAscii = (text.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+      let outputAscii = (text.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+
+      if (text.charCodeAt(i) === 32 ) {
+        outputAscii = " ";
+        result += " ";
+      }
 
       result += String.fromCharCode(outputAscii);
     }  
     return result;
   },
   decode: (text, offset) => {
-    var result = "";
+    let result = "";
 
-    for (var i = 0; i < text.length; i++) {
+    for (let i = 0; i < text.length; i++) {
       //var inputAscii = text.charCodeAt(i);
-      var outputAscii = (text.charCodeAt(i) + 65 - parseInt(offset)) % 26 + 65;//==>(no era -65, era +)
+      let outputAscii = (text.charCodeAt(i) + 65 - parseInt(offset)) % 26 + 65;//==>(no era -65, era +)
+      
+      if (text.charCodeAt(i) === 32 ) {
+        outputAscii = " ";
+        result += " ";
+      }
 
       result += String.fromCharCode(outputAscii);
     }  
